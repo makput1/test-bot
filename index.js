03e19e5dceceea48c7a6e9b452a40b828d143a66
Makput <pkuypkuy6@gmail.com>
2022-11-13 01:55:31 +0700

    1

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
-const { makeWASocket, useSingleFileAuthState, makeInMemoryStore } = require("@adiwajshing/baileys")
-const qrcode = require("qrcode-terminal")
-const pino = require("pino")
+const {
+  default: makeWASocket,
+  useSingleFileAuthState,
+  makeInMemoryStore,
+} = require("@adiwajshing/baileys");
+const pino = require("pino");
+const qrcode = require("qrcode-terminal");
 
+const { state, saveState } = useSingleFileAuthState("./session.json");
 const store = makeInMemoryStore({
-  logger: pino().child({level: "silent", stream: "store"})
-})
-const {state, saveState} = useSingleFileAuthState("./session.json")
+  logger: pino().child({ level: "silent", stream: "store" }),
+});
 
 const startBot = async () => {
   const sock = await makeWASocket({
     printQRInTerminal: false,
-    auth: state
-  })
+    auth: state,
+  });
+  sock.ev.on("connection.update", (update) => {
+    const { connection, lastDisconnect } = update;
+    console.log(connection);
+  });
+  sock.ev.on("creds.update", saveState);
+};
 
-
-}
+startBot();
